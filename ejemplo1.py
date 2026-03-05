bbe540b70faa467b75dd43126adebd2d40d9219c
TwinStars <ander112405@gmail.com>
2026-03-04 22:17:10 -0600

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,7 @@
+a=input("coloque su nivel de poder mágico")
+if a == "imperial":
+  print("Increíble papu :V")
+elif a == "divino":
+  print("Adopteme maestra")
+else:
+  print("hasta las webas pes")
