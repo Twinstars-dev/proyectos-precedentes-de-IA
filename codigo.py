50842705c8fb780b05fb053a10d3d8187637d1fe
Ander <ander112405@gmail.com>
2026-03-06 21:27:57 -0600

    modifique el codigo.py haciendolo más interesante

--- codigo.py
+++ codigo.py
@@ -1,3 +1,3 @@
-nombre="maria"
-
-print("hola",nombre)
+Situación="Si Juanito quiere ir a su casa que esta a 10km, a una velocidad de 1km/h, cuanto le tomaría llegar a su casita?"
+Solucion="Le tomaría 10 horas"
+print(f'En retrospectiva, el siguiente problema : {Situación} la respuesta correcta sería que : {Solucion}')
