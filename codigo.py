e1a6361a4410f50d4aeb19ca16c3f5a4cc4d8511
Ander <ander112405@gmail.com>
2026-03-11 22:09:43 -0600

    modifique solo el codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,3 @@
-Situación="Si Juanito quiere ir a su casa que esta a 10km, a una velocidad de 1km/h, cuanto le tomaría llegar a su casita?"
-Solucion="Le tomaría 10 horas"
-print(f'En retrospectiva, el siguiente problema : {Situación} la respuesta correcta sería que : {Solucion}')
+contexto="en que año se independizo el Perú?"
+respuesta="1821"
+print(f'en retrospectiva, la siguiente pregunta:  {contexto} tiene como solución : {respuesta}')
